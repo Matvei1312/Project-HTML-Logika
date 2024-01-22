f87393720f41a8ca5563bfa74c876152e52b1156
Matvei1312 <144618591+Matvei1312@users.noreply.github.com>
2024-01-22 17:26:41 +0200

    Update runpy.js

--- logica-progect/runpy.js
+++ logica-progect/runpy.js
@@ -1,17 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Отримайте посилання на кнопку
     var startButton = document.getElementById('startButton');
-
-    // Додайте обробник подій для натискання кнопки
     startButton.addEventListener('click', function() {
-        // Тут можна викликати код, який ви хочете виконати при натисканні кнопки
-        // Наприклад, викликати Python-програму через використання AJAX або fetch
-
-        // Приклад виклику Python-скрипта через fetch:
-        fetch('runPythonScript.php') // Замініть на шлях до вашого серверного скрипта
+        fetch('runpy2.php')
             .then(response => response.text())
             .then(data => {
-                console.log(data); // Вивести вивід Python-програми у консоль
+                console.log(data);
             })
             .catch(error => {
                 console.error('Помилка:', error);
