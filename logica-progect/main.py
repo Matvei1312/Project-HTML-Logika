bdb003f675c050b9e5c66de4851bdbfabbf71c50
Matvei1312 <144618591+Matvei1312@users.noreply.github.com>
2024-01-22 17:31:44 +0200

    Update main.py

--- logica-progect/main.py
+++ logica-progect/main.py
@@ -1,1 +1,7 @@
+import random
 
+print(f"random([{random.randint(1, 8479275)])", end="\n")
+print("""Ви щас бачили СЛОЖНЕЙЩИЙ КОД В 2 СТРОЧКИ ось код для рандома:"
+import random
+print(f"random([{random.randint(1, 8479275)])", end="\n")
+"")
